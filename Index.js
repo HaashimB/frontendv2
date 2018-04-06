39b19c34c79b49c0a853c976d5b80e6c049402e3
hashdoge <hashdoggie@gmail.com>
2018-04-06 20:13:33 +0100

    added new visualisation

--- Index.js
+++ Index.js
@@ -6,63 +6,97 @@ var width = 1400;
 var height = 800;
 
 var data = [];
-
+var dataChild = [];
 axios.get("http://127.0.0.1:8000/stack/tags/")
     .then(function(res){
         data = (res.data);
-        console.log(data.map(function(p){return p.count}));
-        createCircle(data);
+        console.log(data.map(function(p){return p.content}));
+        //createCircle(data);
     })
     .catch(function(error){
         console.log(error)
     });
+dataChild = data.map(function(p){return p.content});
 
+var svg = d3.select("svg"),
+    margin = 20,
+    diameter = +svg.attr("width"),
+    g = svg.append("g").attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
 
+var color = d3.scaleLinear()
+    .domain([-1, 5])
+    .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
+    .interpolate(d3.interpolateHcl);
 
+var pack = d3.pack()
+    .size([diameter - margin, diameter - margin])
+    .padding(2);
 
-function createCircle(d){
-    var col;
-    var svg = d3.select('svg')
-        .attr('width', width)
-        .attr('height', height);
-    for(var i = 0;i<d.length;i++){
+d3.json(datachild, function(error, root) {
+    if (error) throw error;
 
-        svg.append('circle')
-            .style('fill', getRandomColor())
-            .style('stroke', 'black')
-            .attr('cx', locationX[i])
-            .attr('cy', locationY[i])
-            .attr('r', (d[i].count)/15)
-            .on('mouseover', function(){
-                col = d3.select(this).style('fill');
-                console.log(d3.select(this).attr('cx') + ',' + d3.select(this).attr('cy'));
-                d3.select(this)
-                    .style('fill', 'orange');
-            })
-            .on('mouseout', function(){
-                d3.select(this).style('fill', col);
-            })
-            .on('click', function(){
-                d3.select(this).style('fill', 'green');
-                col = d3.select(this).style('fill');
+    root = d3.hierarchy(root.content)
+        .sum(function(d) { return d.size; })
+        .sort(function(a, b) { return b.value - a.value; });
+
+    var focus = root,
+        nodes = pack(root).descendants(),
+        view;
+
+    var circle = g.selectAll("circle")
+        .data(nodes)
+        .enter().append("circle")
+        .attr("class", function(d) { return d.parent ? d.children ? "node" : "node node--leaf" : "node node--root"; })
+        .style("fill", function(d) { return d.children ? color(d.depth) : null; })
+        .on("click", function(d) { if (focus !== d) zoom(d), d3.event.stopPropagation(); });
+
+    var text = g.selectAll("text")
+        .data(nodes)
+        .enter().append("text")
+        .attr("class", "label")
+        .style("fill-opacity", function(d) { return d.parent === root ? 1 : 0; })
+        .style("display", function(d) { return d.parent === root ? "inline" : "none"; })
+        .text(function(d) { return d.data.name; });
+
+    var node = g.selectAll("circle,text");
+
+    svg
+        .style("background", color(-1))
+        .on("click", function() { zoom(root); });
+
+    zoomTo([root.x, root.y, root.r * 2 + margin]);
+
+    function zoom(d) {
+        var focus0 = focus; focus = d;
+
+        var transition = d3.transition()
+            .duration(d3.event.altKey ? 7500 : 750)
+            .tween("zoom", function(d) {
+                var i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2 + margin]);
+                return function(t) { zoomTo(i(t)); };
             });
-            svg.append('text')
-                .attr('x', locationX[i])
-                .attr('y', locationY[i])
-                .attr('font-size', d[i].count/100)
-                .text(d[i].name);
 
+        transition.selectAll("text")
+            .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
+            .style("fill-opacity", function(d) { return d.parent === focus ? 1 : 0; })
+            .on("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
+            .on("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
     }
 
-}
-
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+    function zoomTo(v) {
+        var k = diameter / v[2]; view = v;
+        node.attr("transform", function(d) { return "translate(" + (d.x - v[0]) * k + "," + (d.y - v[1]) * k + ")"; });
+        circle.attr("r", function(d) { return d.r * k; });
     }
-    return color;
+});
+
+
+function getRandomColor(upperLim){
+    var color = d3.scaleLinear()
+        .domain([0,upperLim])
+        .range(["#b7d3ff","#1a55b2"]);
+
+    return color(Math.floor(Math.random()* upperLim));
 }
 
 function randomNumGen(num, axis){
