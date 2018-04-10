67e81c839aa1cc826998ed5bfb5cdbe13ebb9dd5
hashdoge <hashdoggie@gmail.com>
2018-04-11 00:23:10 +0100

    .

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var pack = d3.pack()
     .padding(2);
 
 function callAPI() {
-    return axios.get("http://67.207.71.67:8080/stack/tags/");
+    return axios.get("http://67.207.71.67:8888/stack/tags/");
 }
 
 async function loadPage() {
